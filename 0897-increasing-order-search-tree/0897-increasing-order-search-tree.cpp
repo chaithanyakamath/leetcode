fbb573d550caeccaf64dc8bfffb7f6f59d9d56ab
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-06-25 10:24:59 +0530

    Time: 0 ms (100%), Space: 10.9 MB (64.6%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
@@ -11,26 +11,35 @@
  */
 class Solution {
 public:
-    vector<int> store;
+    // vector<int> store;
+    TreeNode* dummy = new TreeNode(0);
+    TreeNode* curr = dummy;
+
     void inorder(TreeNode* root){
         if(!root)   return;
 
         inorder(root->left);
-        store.push_back(root->val);
+
+        // store.push_back(root->val);
+        root->left = nullptr;
+        curr->right = root;
+        curr = root;
+
         inorder(root->right);
     }
 
     TreeNode* increasingBST(TreeNode* root) {
         inorder(root);
+        return dummy->right;
 
-        int n = store.size();
-        TreeNode* temp = new TreeNode(store[0]);
-        TreeNode* curr = temp;
+        // int n = store.size();
+        // TreeNode* temp = new TreeNode(store[0]);
+        // TreeNode* curr = temp;
 
-        for(int i=1; i<n; i++){
-            curr->right = new TreeNode(store[i]);
-            curr = curr->right;
-        }
-        return temp;
+        // for(int i=1; i<n; i++){
+        //     curr->right = new TreeNode(store[i]);
+        //     curr = curr->right;
+        // }
+        // return temp;
     }
 };
