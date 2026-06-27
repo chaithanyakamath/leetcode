c67ba15aa33323194d48f4bd81eb87ffb966858a
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-06-27 15:55:46 +0530

    Time: 0 ms (100%), Space: 9 MB (68.34%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.c
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -1,12 +1,21 @@
 int searchInsert(int* nums, int numsSize, int target) {
-    int count=0;
-    for(int i=0; i<numsSize; i++){
-        if(nums[i]<target){
-            count++;
-        }
-        else{
-            return count;
-        }
+    // int count=0;
+    // for(int i=0; i<numsSize; i++){
+    //     if(nums[i]<target){
+    //         count++;
+    //     }
+    //     else{
+    //         return count;
+    //     }
+    // }
+    // return count;
+
+    int l = 0, r = numsSize-1;
+    while(l<=r){
+        int mid = l + (r-l) / 2;
+        if(nums[mid] == target) return mid;
+        else if( target < nums[mid])    r = mid-1;
+        else l = mid+1;
     }
-    return count;
+    return l;
 }
