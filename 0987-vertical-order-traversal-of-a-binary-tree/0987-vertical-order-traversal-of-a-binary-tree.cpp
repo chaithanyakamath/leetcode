98addc4c08a8e13a7946c524e67e70ee231c87a4
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-07-03 12:20:59 +0530

    Time: 0 ms (100%), Space: 15.3 MB (91.44%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, int col, int row, vector<tuple<int, int, int>> &store){
+        if(!root)   return;
+        store.emplace_back(col,row, root->val); // emplace_back() to push multiple attributes
+
+        dfs(root->left, col-1, row+1, store);
+        dfs(root->right, col+1, row+1, store);
+    }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<tuple<int, int, int>> store; // col, row, root->val
+        dfs(root, 0, 0, store);
+
+        sort(store.begin(), store.end());
+        vector<vector<int>> ans;
+        int prevCol = INT_MIN;
+        for(auto[col, row, val] : store){
+            if(prevCol != col){
+                ans.push_back({});
+                prevCol = col;
+            }
+            ans.back().push_back(val);
+        }
+        return ans;
+    }
+};
