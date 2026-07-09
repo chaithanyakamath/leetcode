af2d946dd58ebf110ecdd208ad65eb8b1142551a
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-07-09 19:13:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (36.92%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> dp;
+    int solve(int n){
+        if(n <= 2)  return n;
+        if(dp[n] != -1) return dp[n];
+        else return dp[n] = solve(n-1) + solve(n-2);
+    }
+    int climbStairs(int n) {
+        dp.assign(n+1, -1);
+        return solve(n);
+    }
+};
