1d801e57306635661c48c034a6a55ecff535f23e
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-07-09 18:31:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.73%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
+    vector<int> dp;
     int solve(int n){
         if(n <= 1)  return n;
-        else    return solve(n-1) + solve(n-2); 
+        if(dp[n] != -1) return dp[n];
+        else    return dp[n] = solve(n-1) + solve(n-2); 
     }
     int fib(int n) {
+        dp.assign(n+1, -1);
         return solve(n);
     }
 };
