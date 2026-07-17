d8b3fbd91944c4922d94004179a0ee5c739aecb4
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-07-17 15:48:10 +0530

    Time: 0 ms (100%), Space: 16.2 MB (54.13%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    vector<int> dp;
+    vector<int> dp; // initialize
     int solve(vector<int> &store, int i){
         if(i >= store.size())   return 0;
 
-        if(dp[i] != -1) return dp[i];
+        if(dp[i] != -1) return dp[i]; // check
         int take = store[i] + solve(store, i+2);
         int skip = solve(store, i+1);
 
-        return dp[i] = max(take, skip);
+        return dp[i] = max(take, skip); // store
     }
     int deleteAndEarn(vector<int>& nums) {
         int maxx = *max_element(nums.begin(), nums.end());
         vector<int> store(maxx+1,0);
+
         for(int n : nums)      store[n] += n;
-        dp.assign(store.size()+1, -1);
+        dp.assign(store.size()+1, -1); // declare
 
         return solve(store,0);
     }
