18ebd87dce6bc72393285d418103e94d800dcace
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-07-23 18:57:19 +0530

    Time: 435 ms (7.98%), Space: 163.7 MB (5.41%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    vector<vector<int>> dp; // 2d dp ==> i & n(n-store[i])
     int solve(vector<int>& store, int i, int n){
         if(n == 0)  return 0;
         if(i == store.size())   return 1e9;
