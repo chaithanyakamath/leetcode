b841cef4076ea62f1b8579585089a6069a9285bb
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-07-23 18:44:36 +0530

    Time: 441 ms (7.79%), Space: 163.6 MB (5.74%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+vector<vector<int>> dp;
+    int solve(vector<int>& store, int i, int n){
+        if(n == 0)  return 0;
+        if(i == store.size())   return 1e9;
+
+        if(dp[i][n] != -1) return dp[i][n];
+        int take = INT_MAX;
+        if(n >= store[i])   take = 1 + solve(store, i, n - store[i]);
+        int skip = solve(store, i+1, n);
+
+        return dp[i][n] = min(take, skip);
+    }
+    int numSquares(int n) {
+        vector<int> store;
+        for(int i=1; (i*i)<=n; i++){
+            store.push_back(i*i);
+        }
+        int m = store.size();
+        dp.assign(m+1, vector<int>(n+1, -1));
+        return solve(store,0,n);
+    }
+};
