2a0f7b006364e302332e61bf6c406c5cbb856782
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-08-21 23:13:54 +0530

    Time: 3 ms (75.85%), Space: 11.9 MB (69.39%) - LeetHub

---
+++ 1117-building-h2o/1117-building-h2o.cpp
@@ -0,0 +1,27 @@
+class H2O {
+public:
+    mutex m;
+    condition_variable cv;
+    int turn;
+    H2O() {
+        turn  = 0;
+    }
+
+    void hydrogen(function<void()> releaseHydrogen) {
+        unique_lock<mutex> lock(m);
+        while(turn == 2)    cv.wait(lock);
+
+        releaseHydrogen();
+        ++turn;
+        cv.notify_all();
+    }
+
+    void oxygen(function<void()> releaseOxygen) {
+        unique_lock<mutex> lock(m);
+        while(turn < 2) cv.wait(lock);
+
+        releaseOxygen();
+        turn = 0;
+        cv.notify_all();
+    }
+};
