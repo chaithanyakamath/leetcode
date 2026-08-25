44648212afc1c5e837ae419f8a7ce3c5123d2a20
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-08-25 17:31:53 +0530

    Time: 0 ms (100%), Space: 9.9 MB (77.39%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        int l = 0;
+        int r = n-1;
+        // string a="", b="";
+        while (l < r) {
+            // Skip non-alphanumeric
+            while (l < r && !isalnum(s[l])) l++;
+            while (l < r && !isalnum(s[r])) r--;
+
+            if (tolower(s[l]) != tolower(s[r])) return false;
+
+            l++;
+            r--;
+        }
+        return true;
+    }   
+};
