aefdee53f8cdc91439b72187ec19db00d466566a
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-08-31 21:20:50 +0530

    Time: 0 ms (100%), Space: 14.2 MB (56.98%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-       int n = prices.size();
-       vector<int> ans(n);
+        int n = prices.size();
+        vector<int> ans(n);
+        stack<int> st;
 
-        for(int i=0; i<n; i++){
-            ans[i] = prices[i];
-            for(int j=i+1; j<n; j++){
-                if(j>i && prices[j] <= prices[i]){
-                    ans[i] = prices[i] - prices[j];
-                    break;
-                }
-            }
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && st.top() > prices[i]) st.pop();
+
+            if(!st.empty()) ans[i] = prices[i] - st.top();
+            else ans[i] = prices[i];
+
+            st.push(prices[i]);
         }
+
+        // for(int i=0; i<n; i++){
+        //     ans[i] = prices[i];
+        //     for(int j=i+1; j<n; j++){
+        //         if(j>i && prices[j] <= prices[i]){
+        //             ans[i] = prices[i] - prices[j];
+        //             break;
+        //         }
+        //     }
+        // }
         return ans;
     }
 };
