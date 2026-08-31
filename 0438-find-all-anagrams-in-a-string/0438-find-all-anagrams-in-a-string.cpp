d620ad1c33cc4609f802525ce167c0a03848e87e
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-08-31 19:47:58 +0530

    Time: 5 ms (58.15%), Space: 11.5 MB (85.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    bool check(vector<int>& freq){
-        for(int &n : freq) {
+    bool check(vector<int>& mp){
+        for(int n : mp){
             if(n != 0)  return false;
         }
         return true;
     }
-
     vector<int> findAnagrams(string s, string p) {
         int n = s.size();
         int m = p.size();
+        vector<int> mp(26,0);
         vector<int> ans;
-        vector<int> freq(26, 0);
 
-        for(char c : p) freq[c-'a']++;
-
-        int i=0, j=0;
-        while(j<n){
-            freq[s[j]-'a']--;
-            if(j-i+1 == m) {
-                if(check(freq))    ans.push_back(i);
-                freq[s[i]-'a']++;
-                i++;
+        for(char c : p) mp[c-'a']++;
+        int l = 0, r = 0;
+        while(r<n){
+            mp[s[r]-'a']--;
+            if(r-l+1 == m){
+                if(check(mp))  ans.push_back(l);
+                mp[s[l]-'a']++;
+                l++;
             }
-            j++;
+            r++;
         }
         return ans;
     }
