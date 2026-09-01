3593ca4b9914b95b043068751e08b3880486f7f6
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-01 19:17:06 +0530

    Time: 11 ms (15.16%), Space: 28.6 MB (39.36%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -6,15 +6,9 @@ public:
         vector<int> ans(n,0);
 
         for(int i=2*n-1; i>=0; i--){
-            int cur = nums[i%n];
-
-            while (!st.empty() && st.top() <= cur){ 
-                st.pop(); 
-            }
-
-            if(i<n){
-                ans[i] = st.empty() ? -1 : st.top();
-            }
+            int cur = nums[i%n]; // gives i in range of(0,n)
+            while(!st.empty() && st.top() <= cur)   st.pop();
+            if(i<n) ans[i] = (st.empty())? -1 : st.top();
 
             st.push(cur);
         }
