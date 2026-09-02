c963c9d8ccbbc16ea8bc3d9aebde28a007cf67b8
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-02 23:02:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (39.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<int> dp;
+    vector<int> dp; // declare
     int solve(int n){
         if(n <= 2)  return n;
-        if(dp[n] != -1) return dp[n];
-        else return dp[n] = solve(n-1) + solve(n-2);
+        if(dp[n] != -1) return dp[n]; // if calculated
+        return dp[n] = solve(n-1) + solve(n-2); //store
     }
     int climbStairs(int n) {
-        dp.assign(n+1, -1);
+        dp.assign(n+1, -1); // initiaize
         return solve(n);
     }
 };
