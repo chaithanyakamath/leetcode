5408d75300a93030754568383b99f4011d978e41
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-03 15:19:45 +0530

    Time: 15 ms (28.18%), Space: 18.5 MB (16.09%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -3,12 +3,13 @@ public:
 int n;
 vector<int> dp;
     bool solve(int start, string s, vector<string>& dict){
-        if(start == n)  return true;
+        if(start == n)  return true; // complete string is done building
         if(dp[start] != -1) return dp[start];
-        for(int len = 1; start+len <= n; len++){
-            string cur = s.substr(start, len);
+        for(int len = 1; start+len <= n; len++){ // len of substr
+            string cur = s.substr(start, len); // get a substr from 'start' idx of 'len' length
             if(find(dict.begin(), dict.end(), cur) != dict.end()){    
-                if(solve(start+len, s, dict))   return dp[start] = true;
+                if(solve(start+len, s, dict))   return dp[start] = true; 
+                // idx will be 'start+len'
             }
         }
         return dp[start] = false;
