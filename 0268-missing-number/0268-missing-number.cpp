413eeaa2dd5976cde0e26c7f5508c7a5a2394be3
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-04 18:19:59 +0530

    Time: 0 ms (100%), Space: 21.7 MB (93.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         int missing = 0;
 
-        for(int i=0; i<n; i++)  missing ^= nums[i];
-        for(int i=1; i<=n; i++)  missing ^= i;
+        for(int i=0; i<n; i++)  missing ^= nums[i]; // xor with all values in nums
+        for(int i=1; i<=n; i++)  missing ^= i; // xor with values in range [0,n] inclusively which gives u the missing one's
 
         return missing;
     }
