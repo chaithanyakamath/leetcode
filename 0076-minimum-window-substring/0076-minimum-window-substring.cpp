8783619f4c66d756498788cf82c9cee241d6bea9
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-08 18:20:18 +0530

    Time: 25 ms (15.12%), Space: 11.5 MB (64.85%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    bool check(vector<int>& freq){
-        for(int n : freq){
-            if(n > 0)  return false;
+    bool check(vector<int>& store){
+        for(int n : store){
+            if(n > 0)   return false;
         }
         return true;
     }
     string minWindow(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<int> freq(128, 0);
+        vector<int> store(128, 0);
 
-        for(char c : t) freq[c]++;
+        for(char c : t) store[c]++;
 
-        int i=0, j=0;
-        int start = 0;
-        int len = INT_MAX;
-        string ans = "";
-        while(j < n){
-            freq[s[j]]--;
-
-            while(check(freq)){   
-                if(j-i+1 < len){
-                    len = j-i+1;
-                    start = i;
+        int l = 0, len = INT_MAX, start = 0;
+        for(int r=0; r<n; r++){
+            store[s[r]]--;
+            while(check(store)){
+                if(len > r-l+1){ 
+                    len = r-l+1;
+                    start = l;
                 }
-                freq[s[i]]++;
-                i++;
+                store[s[l]]++;
+                l++;
             }
-            j++;
         }
-        return (len == INT_MAX)? "" : s.substr(start,len);
+        return (len == INT_MAX)? "" : s.substr(start, len);
     }
 };
