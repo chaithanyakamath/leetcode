6fa51830daddff8e2f863d1844f86b8857469e21
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-09 19:43:25 +0530

    Time: 0 ms (100%), Space: 13 MB (89.61%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -5,17 +5,14 @@ public:
         bool vowels[26] = {false};
         vowels[0] = vowels[4] = vowels[8] = vowels[14] = vowels[20] = true;
 
-        int j = 0, i = 0;
-        int ans = 0;
-        int count;
-        while(j<n){
-            if(vowels[s[j]-'a'])    count++;
-            if(j-i+1 == k){
-                ans = max(ans, count);
-                if(vowels[s[i]-'a']) count--;
-                i++;
+        int l = 0, ans = 0, count = 0;
+        for(int r=0; r<n; r++){
+            if(vowels[s[r]-'a'])    count++;
+            while(r-l+1 > k){
+                if(vowels[s[l]-'a'])    count--;
+                l++;
             }
-            j++;
+            ans = max(ans, count);
         }
         return ans;
     }
