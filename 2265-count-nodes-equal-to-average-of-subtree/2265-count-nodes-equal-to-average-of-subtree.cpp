ee13173a3f027a11281699333214bf78f8d8b015
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-10 10:14:39 +0530

    Time: 0 ms (100%), Space: 15.8 MB (29.76%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -1,32 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int ans = 0;
-    pair<int, int> dfs(TreeNode* root){
-        if(!root)   return {0,0}; //root with no children
+    int count = 0;
 
-        auto lsub = dfs(root->left); // left subtree till leaf of left subtree
-        auto rsub = dfs(root->right); // right subtree till leaf of right subtree
+    int solve(TreeNode* root, int &rootCount){
+        if(!root){
+            rootCount = 0;
+            return 0;
+        }
 
-        int sum = lsub.first + rsub.first + root->val; // compute sum of each node's subtree along with its value
-        int count = lsub.second + rsub.second + 1; // compute count of nodes in its subtree along with itself
+        int lrootCount = 0;
+        int rrootCount = 0;
 
-        if(sum/count == root->val) ans++; // main comparision
-        
-        return{sum, count};
+        int l = solve(root->left, lrootCount);
+        int r = solve(root->right, rrootCount);
+
+        rootCount = lrootCount + rrootCount + 1;
+
+        int sum = root->val + l + r;
+        int avg = sum / rootCount;
+
+        if(avg == root->val)
+            count++;
+
+        return sum;
     }
+
     int averageOfSubtree(TreeNode* root) {
-        dfs(root);
-        return ans;
+        if(!root)
+            return 0;
+
+        int rootCount = 0;
+        solve(root, rootCount);
+
+        return count;
     }
 };
