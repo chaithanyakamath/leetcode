0b4ddf40147a5918953c4c8cd41fc0b3213125b9
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-10 15:08:40 +0530

    Time: 0 ms (100%), Space: 19.5 MB (27.38%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,24 +10,18 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* ans = new ListNode(0);
-        ListNode* copy = ans;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1)  return l2; // l1 is null so return l2 
+        if(!l2)  return l1; // l2 is null so return l1
 
-        while(list1 && list2){
-            if(list1->val > list2->val){
-                copy->next = list2;
-                list2 = list2->next;
-            }
-            else{
-                copy->next = list1;
-                list1 = list1->next;
-            }
-            copy = copy->next;
+        if(l1->val <= l2->val){  
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
         }
-        if(list1)   copy->next = list1;
-        if(list2)   copy->next = list2;
-
-        return ans->next;
+        else{    
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+        // whosever value is less at that particular recursion lvl that is return n its next pointer will point for the continuation of the recursion
     }
 };
