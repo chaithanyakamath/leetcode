553c761f27923416a51e82ea6727a63b9a217bc6
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-11 09:12:40 +0530

    Time: 0 ms (100%), Space: 14.4 MB (87.72%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    void bfs(int node, vector<vector<int>>& adj, vector<bool>& vis){
-        queue<int> qu;
-        qu.push(node);
+    // void bfs(int node, vector<vector<int>>& adj, vector<bool>& vis){
+    //     queue<int> qu;
+    //     qu.push(node);
+    //     vis[node] = true;
+
+    //     while(!qu.empty()){
+    //         int cur = qu.front();
+    //         qu.pop();
+
+    //         for(auto neigh : adj[cur]){
+    //             if(!vis[neigh]){
+    //                 qu.push(neigh);
+    //                 vis[neigh] = true;
+    //             }
+    //         }
+    //     }
+    // }
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& vis){
         vis[node] = true;
-
-        while(!qu.empty()){
-            int cur = qu.front();
-            qu.pop();
-
-            for(auto neigh : adj[cur]){
-                if(!vis[neigh]){
-                    qu.push(neigh);
-                    vis[neigh] = true;
-                }
-            }
+        for(auto neigh : adj[node]){
+            if(!vis[neigh]) dfs(neigh, adj, vis);
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
         vector<bool> vis(n,false);
-        bfs(0, rooms, vis);
+        dfs(0, rooms, vis);
         for(int i=0; i<n; i++){
             if(!vis[i]) return false;
         }
