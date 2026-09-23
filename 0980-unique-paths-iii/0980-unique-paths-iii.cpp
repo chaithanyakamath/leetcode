23028dfb30428315606e3b1b0dc364b1c0d7ff9f
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-23 14:30:28 +0530

    Time: 0 ms (100%), Space: 9.4 MB (27.85%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+int n, m, valid = 0, ans = 0;
+pair<int, int> loc = {0,0};
+
+    void solve(int i, int j, vector<vector<int>>& grid, int valid){
+        if(i<0 || i>=n || j<0 || j>= m) return;
+
+        if(grid[i][j] == 2 && valid == 0){
+            ans++;
+            return;
+        }
+        if(grid[i][j] == 0 || grid[i][j] == 1){
+            int org = grid[i][j];
+            grid[i][j] = -1;
+            valid--;
+            solve(i+1, j, grid, valid);
+            solve(i-1, j, grid, valid);
+            solve(i, j+1, grid, valid);
+            solve(i, j-1, grid, valid);
+            grid[i][j] = org;
+        }
+    }
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 0 || grid[i][j] == 1)  valid++;
+                if(grid[i][j] == 1){
+                    loc.first = i;
+                    loc.second = j;
+                } 
+            }
+        }
+        solve(loc.first, loc.second, grid, valid);
+        return ans;
+    }
+};
