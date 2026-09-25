4e7a4ac75dffd83d48d82e2d3257096cc1cce126
Chaithanya N Kamath <157230384+chaithanyakamath@users.noreply.github.com>
2026-09-25 16:14:38 +0530

    Time: 163 ms (37.91%), Space: 56.1 MB (28.35%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int len;
+vector<vector<vector<int>>> dp;
+    int solve(int i, int n, int minProfit, vector<int>& group, vector<int>& profit){
+        if(i == len)   return minProfit <= 0;
+        if(dp[i][n][minProfit] != -1)  return dp[i][n][minProfit];
+
+        int skip = solve(i+1, n, minProfit, group, profit);
+        int take = 0;
+        if(n >= group[i]){
+            take = solve(i+1, n-group[i], max(0,minProfit-profit[i]), group, profit);
+        }
+        return dp[i][n][minProfit] = (take + skip) % (1000000007);
+    }
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        len = group.size();
+        dp.assign(len, vector<vector<int>>(n+1, vector<int>(minProfit+1, -1)));
+
+        int ans = solve(0, n, minProfit, group, profit);
+        return ans;
+    }
+};
